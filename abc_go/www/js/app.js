7df7495e81543539fc1af89ad98bf956c8c824a5
binhninhlanabcgo <binhninhlanabcgo@gmail.com>
2015-09-08 11:59:49 +0700

    Start new code

---
+++ abc_go/www/js/app.js
@@ -0,0 +1,96 @@
+angular.module('abcGoApp', ['ionic']);
+
+angular.module('abcGoApp').controller('AppCtrl', function() {
+ ionic.Platform.ready(function() {
+
+  });
+
+});
+//Navigate
+angular.module('abcGoApp').config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('menu', {
+      url: "/menu",
+      abstract: true,
+      templateUrl: "component/menu.html",
+      controller: 'MenuCtrl'
+    })
+    .state('menu.tabs', {
+      url: "/tab",
+      views: {
+        'menuContent' :{
+          templateUrl: "component/tabs.html"
+        }
+      }
+    })
+    .state('menu.tabs.buttons', {
+      url: "/buttons",
+      views: {
+        'buttons-tab': {
+          templateUrl: "component/buttons.html",
+          controller: 'ButtonsTabCtrl'
+        }
+      }
+    })
+    .state('menu.tabs.list', {
+      url: "/list",
+      views: {
+        'list-tab': {
+          templateUrl: "component/list.html",
+          controller: 'ListCtrl'
+        }
+      }
+    })
+    .state('menu.tabs.item', {
+      url: "/item",
+      views: {
+        'list-tab': {
+          templateUrl: "component/item.html"
+        }
+      }
+    })
+    .state('menu.tabs.form', {
+      url: "/form",
+      views: {
+        'form-tab': {
+          templateUrl: "component/form.html"
+        }
+      }
+    })
+    .state('menu.keyboard', {
+      url: "/keyboard",
+      views: {
+        'menuContent': {
+          templateUrl: "component/keyboard.html"
+        }
+      }
+    })
+    .state('menu.slidebox', {
+      url: "/slidebox",
+      views: {
+        'menuContent': {
+          templateUrl: "component/slidebox.html",
+          controller: 'SlideboxCtrl'
+        }
+      }
+    })
+    .state('menu.about', {
+      url: "/about",
+      views: {
+        'menuContent': {
+          templateUrl: "component/about.html"
+        }
+      }
+    })
+    .state('menu.dash', {
+      url: "/dash",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/dash_board.html"
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise("menu/dash");
+
+});
---
+++ abc_go/www/js/controllers_demo.js
@@ -0,0 +1,103 @@
+angular.module('abcGoApp').controller('ListCtrl', function ($scope) {
+
+  $scope.data = {
+    showDelete: false
+  };
+
+  $scope.itemButtons = [
+    {
+      text: 'Delete',
+      type: 'button-assertive',
+      onTap: function (item) {
+        alert('Delete Item: ' + item.id + ' ?');
+      }
+    }
+  ];
+
+  $scope.onItemDelete = function (item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+
+  $scope.items = [
+    {
+      id: 1
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3
+    },
+    {
+      id: 4
+    },
+    {
+      id: 5
+    },
+    {
+      id: 6
+    },
+    {
+      id: 7
+    },
+    {
+      id: 8
+    },
+    {
+      id: 9
+    },
+    {
+      id: 10
+    }
+  ];
+
+});
+
+angular.module('abcGoApp').controller('ButtonsTabCtrl', function ($scope, $ionicPopup, $ionicActionSheet, $ionicModal) {
+    $scope.showPopup = function () {
+     $ionicPopup.alert({
+       title: 'Popup',
+       content: 'This is ionic popup alert!'
+     });
+    };
+    $scope.showActionsheet = function () {
+        $ionicActionSheet.show({
+          titleText: 'Ionic ActionSheet',
+          buttons: [
+            {
+              text: 'Facebook'
+            },
+            {
+              text: 'Twitter'
+            },
+          ],
+          destructiveText: 'Delete',
+          cancelText: 'Cancel',
+          cancel: function () {
+            console.log('CANCELLED');
+          },
+          buttonClicked: function (index) {
+            console.log('BUTTON CLICKED', index);
+            return true;
+          },
+          destructiveButtonClicked: function () {
+            console.log('DESTRUCT');
+            return true;
+          }
+        });
+    };
+});
+
+angular.module('abcGoApp').controller('SlideboxCtrl', function($scope, $ionicSlideBoxDelegate) {
+  $scope.nextSlide = function() {
+    $ionicSlideBoxDelegate.next();
+  }             
+});              
+
+angular.module('abcGoApp').controller('MenuCtrl', function($scope, $ionicSideMenuDelegate, $ionicModal) {              
+  $ionicModal.fromTemplateUrl('component/modal.html', function (modal) {
+    $scope.modal = modal;
+  }, {
+    animation: 'slide-in-up'
+  });
+ });
